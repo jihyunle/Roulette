0e0941a3f56dfae56fcdfd2b9e4d825e561fe7b7
jihyunle <jennifer.jlee@outlook.com>
2019-02-01 13:51:35 -0500

    Debugged newline character issues and Cleaned overall look

---
+++ README.md
@@ -0,0 +1,19 @@
+# Week One Level Two Challenge - Roulette 
+
+You will write a Java program to simulate Roulette, a casino game.
+
+Players will be able to enter in the amount of money they want to bet and the type of bet they will be placing.
+
+The types of bets include:
+Red: You are betting that the next roll will come up red.
+Black: You are betting that the next roll will come up black.
+Even: This is wagering that the ball will land on an even number between 1-36.
+Odd: This is wagering that the ball will land on an odd number between 1-36.
+Low: This is a bet that the next number will be between 1 and 18.
+High: This is a bet that the next number will be between 19 and 36.
+
+The program will then randomly generate a number between 1-36 and a color (red or black). You will then double the player's amount if they won, or deduct their money if they lost.
+
+Allow the user to play as many games as they want until they want to quit.
+
+At the end of the program, print out the net amount of money they've earned, whether that's positive or negative. In addition, also print out the number of games they have won and lost.
--- src/Roulette.java
+++ src/Roulette.java
@@ -1,55 +1,92 @@
+/*
+* You will write a Java program to simulate Roulette, a casino game.
+
+Players will be able to enter in the amount of money they want to bet and the type of bet they will be placing.
+
+The types of bets include:
+Red: You are betting that the next roll will come up red.
+Black: You are betting that the next roll will come up black.
+Even: This is wagering that the ball will land on an even number between 1-36.
+Odd: This is wagering that the ball will land on an odd number between 1-36.
+Low: This is a bet that the next number will be between 1 and 18.
+High: This is a bet that the next number will be between 19 and 36.
+
+The program will then randomly generate a number between 1-36 and a color (red or black). You will then double the player's amount if they won, or deduct their money if they lost.
+
+Allow the user to play as many games as they want until they want to quit.
+
+At the end of the program, print out the net amount of money they've earned, whether that's positive or negative. In addition, also print out the number of games they have won and lost.
+*/
 import java.util.Scanner;
 
 public class Roulette {
 
     public static void main(String[]args){
         Scanner keyboard = new Scanner(System.in);
+
+        // Declare variables (in order of appearance)
         double betAmount = 0;
         String betType = "";
         int num = 0;
         String color = "";
         boolean win;
         double netAmount = 0;
-        int numWins = 0;
-        int numLosses = 0;
+        int roundsWon = 0;
+        int roundsLost = 0;
 
+        // Show welcome message
         showWelcomeMessage();
 
         while (true){
+            // Prompt user for input
             System.out.print("Enter the type of bet you would like to place (red|black|even|odd|high|low): ");
             betType = keyboard.nextLine();
             System.out.print("Enter in your bet amount: ");
             betAmount = keyboard.nextDouble();
 
+            /*
+            * Consume remaining new line character.
+            * https://christprogramming.wordpress.com/2014/01/30/java-common-mistakes-1/*/
+            keyboard.nextLine();
+
+            // Simulate a round of Roulette by generating random number and a random color
             num = generateNum();
             color = generateColor();
 
+            // Display where the ball landed
             System.out.println("The ball landed on " + num + " " + color + ".");
 
+            // Determine whether the user won
             win = analyzeWinUsingSwitch(betType, color, num);
 
+            /*
+            * Display the result, update the net amount (+/-),
+            * and keep counts of wins and losses*/
             if (win){
-                System.out.println("Congratulations, you've won.");
+                System.out.println("Congratulations, you've won. \n");
                 netAmount += betAmount * 2;
-                numWins++;
+                roundsWon++;
             } else {
-                System.out.println("Sorry, you love this bet.");
+                System.out.println("Sorry, you've lost this bet. \n");
                 netAmount -= betAmount;
-                numLosses++;
+                roundsLost++;
             }
 
-            System.out.println("You currently have: $" + netAmount + "\n");
+            System.out.printf("You currently have: $" + "%.2f%n", netAmount);
+            System.out.println();
 
+            // Ask user if they want to continue
             if (!playAgain()){
                 break;
             }
+        } // End of while loop
 
-        }
-
-        showExitMessage(netAmount, numWins, numLosses);
-        keyboard.close();
+        // Show exit message
+        showExitMessage(netAmount, roundsWon, roundsLost);
+//        keyboard.close();
         System.exit(0);
     }
+
 
     public static void showWelcomeMessage(){
         System.out.println("WELCOME! Let's Play some Roulette! \n");
@@ -69,50 +106,6 @@ public class Roulette {
         }
     }
 
-    /*
-    * Analyze win using if statements
-    * */
-    public static boolean analyzeWinUsingIf(String bet, String clr, int num){
-        // Analyze color
-        if (bet.equalsIgnoreCase(clr)){
-            return true;
-        }
-//        else {
-//            return false;
-//        }
-
-        // Analyze number
-        if (bet.equalsIgnoreCase("even") && num%2==0){
-            return true;
-        }
-//        else {
-//            return false;
-//        }
-
-        if (bet.equalsIgnoreCase("odd") && num%2!=0){
-            return true;
-        }
-//        else {
-//            return false;
-//        }
-
-        if (bet.equalsIgnoreCase("low") && num<19){
-            return true;
-        }
-//        else {
-//            return false;
-//        }
-
-        if (bet.equalsIgnoreCase("high") && num>=19){
-            return true;
-        }
-
-        else {
-            return false;
-        }
-
-
-    }
 
     /*
      * Analyze win using switch statements
@@ -123,51 +116,39 @@ public class Roulette {
             case "red":
                 if (bet.equalsIgnoreCase(clr)){
                     return true;
-//                } else {
-//                    return false;
                 }
                 break;
             case "black":
                 if (bet.equalsIgnoreCase(clr)){
                     return true;
-//                } else {
-//                    return false;
                 }
                 break;
             case "even":
                 if (num%2==0){
                     return true;
-//                } else {
-//                    return false;
                 }
                 break;
             case "odd":
                 if (num%2!=0){
                     return true;
-//                } else {
-//                    return false;
                 }
                 break;
             case "low":
                 if (num<19){
                     return true;
-//                } else {
-//                    return false;
                 }
                 break;
             case "high":
                 if (num>=19){
                     return true;
-//                } else {
-//                    return false;
                 }
                 break;
-        }
+        } // End of switch statement
         return false;
     }
 
     public static boolean playAgain(){
-        System.out.println("Would you like to play again? (true|false)");
+        System.out.print("Would you like to play again? (true|false) ");
         Scanner keyboard = new Scanner(System.in);
         String reply = keyboard.nextLine().toLowerCase();
         if (reply.equals("true")){
@@ -178,8 +159,9 @@ public class Roulette {
     }
 
     public static void showExitMessage(double net, int wins, int losses){
+        System.out.println();
         System.out.println("Thank you for playing!");
-        System.out.println("This is how much money you have remaining: $" + net);
+        System.out.printf("This is how much money you have remaining: $" + "%.2f%n", net);
         System.out.println("You've won " + wins + " game(s) and lost " + losses + " game(s).");
     }
 
